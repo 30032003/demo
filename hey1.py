dfdd117d8c2ca80cd7c22695ba6d8ac6ac5e242e
Harshal Dahiwale <97493907+30032003@users.noreply.github.com>
2026-05-02 00:24:08 +0530

    Add manual edit message to hey1.py
    Added a new print statement indicating manual editing.

--- hey1.py
+++ hey1.py
@@ -2,3 +2,5 @@ print("This is harshal from cdac")
 print("I am very talented. and i am listening song right now...")
 
 print("ok.......")
+
+print("This is manually edited by me -- on github only.")
